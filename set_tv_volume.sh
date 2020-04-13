3a1649c71df263af03eacf3d9151cfa790379c72
Tom Howe <tom.rr.howe@gmail.com>
2020-04-13 14:32:55 +0100

    Update set_tv_volume.sh

--- set_tv_volume.sh
+++ set_tv_volume.sh
@@ -2,7 +2,7 @@
 
 echo "setting TV volume to $1"
 
-let "vol = 100 - $1"
-echo $vol > /dev/ttyUSB0
+#let "vol = 100 - $1"
+echo $1 > /dev/ttyUSB0
 
-echo "set TV attenuation to $vol."
+#echo "set TV attenuation to $vol."
